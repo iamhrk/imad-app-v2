18c8931c4e4d581ff324b725c891a37c171d3987
HARIKRISHNAN <iamhrk8@gmail.com>
2017-02-16 14:49:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/Article-one', function (req,res) {
+app.get('/Biography', function (req,res) {
    res.sendFile(path.join(__dirname, 'ui', 'Biography.html'));
 });
 
