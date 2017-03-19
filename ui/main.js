363bfd553444f694138e9226ab0c12ecc0e46a23
HARIKRISHNAN <iamhrk8@gmail.com>
2017-03-19 10:46:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,24 @@
-console.log('Loaded!');
-//move the image
-
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function(){
-    var interval = setInterval(moveRight, 50);
+//counter code
+var button = document.gerElementById('counter');
+button.onclick = function (){
+    //create a request object
+    
+    var request = new XMLHttpRequest();
+    
+    //capture response and store it in a variable
+    
+    request.onreadystatechange = function() {
+        if(request.readystate ===XMLHttpRequest.DONE){
+            if (request.status===200) {
+                var counter =request.responseText;
+                var span =document.getElementById('count');
+                span.InnerHTML=counter.toString();
+            }
+        }
+    //no else part yet made    
+    };
+    
+    //make request
+    request.open('GET','http://iamhrk.imad.hasura-app.io/counter',true);
+    request.send(null);    
 };
