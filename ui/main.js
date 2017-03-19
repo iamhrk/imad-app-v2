554acaf94aec530d3f4ded1797a7099c113a6eed
HARIKRISHNAN <iamhrk8@gmail.com>
2017-03-19 11:29:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,7 +12,7 @@ button.onclick = function (){
             if (request.status===200) {
                 var counter =request.responseText;
                 var span =document.getElementById('count');
-                span.InnerHTML=counter.toString();
+                span.InnerHTML=counter;
             }
         }
     //no else part yet made    
